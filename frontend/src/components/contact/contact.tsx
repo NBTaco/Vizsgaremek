ebd2f52395a875393d75fce92180e12eb749f453
NBTaco <menandor119@gmail.com>
2026-01-08 19:11:10 +0100

    Rolunk oldal, aboutus komponens elkészítése. Title komponens kész. contact, products komponens létrehozva

---
+++ frontend/src/components/contact/contact.tsx
@@ -0,0 +1,15 @@
+import Footer from "../footer/Footer"
+import Header from "../header/Header"
+import Title from "../title/title"
+
+export default function Contact() {
+    return (
+      <div>
+        <Header />
+        <Title titlemessage="Kapcsolat" />
+        <h1>Kapcsolat</h1>
+        <p>Ez a kapcsolat oldal</p>
+        <Footer />
+      </div>
+    )
+}
